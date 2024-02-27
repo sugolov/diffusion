4911046992b06f864c969d46588c6666c8c97d5a
3xela <alexander.neagoe@gmail.com>
2024-02-27 15:16:15 -0500

    alex

--- test/test_ddpm.py
+++ test/test_ddpm.py
@@ -10,8 +10,10 @@ def test_cifar10_load():
     """
     generator = cifar10.data_batch_generator()
     image, label = next(generator)
+
+    print(image.shape)
     im = Image.fromarray(image)
-    im.save("test/out/cifar10_test.jpg")
+    im.save("../test/out/cifar10_test.jpg")
 
 
 
