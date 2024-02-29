6d0e4c6f2eefc91f1b7ac7717f54efa02aa1f1e9
anton <asugolov@gmail.com>
2024-02-28 21:33:58 -0500

    unet fixing

--- models/ddpm.py
+++ models/ddpm.py
@@ -41,44 +41,77 @@ class DDPM(nn.Module):
 
 # TODO: implement a U-Net here, preferrably without diffusers.UNet
 
-class UNetLayer(nn.Module):
+class UNetConvLayer(nn.Module):
 
-    def __init__(self, in_channels, out_channels, kernel_size=2, padding=0, *args, **kwargs):
+    def __init__(self, in_channels, out_channels, kernel_size=2, padding="same", *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.conv1 = Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding)
         self.conv2 = Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding)
-        self.maxpool = MaxPool2d(kernel_size=2, stride=2)
 
     def forward(self, x):
-        x1 = ReLU(self.conv1(x))
-        x2 = ReLU(self.conv2(x1))
-        x = x + x2
-        return x
+        x1 = ReLU()(self.conv1(x))
+        x2 = ReLU()(self.conv2(x1))
+        return x2
+
+class UNetEncLayer(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size=2, padding="same", *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.conv = UNetConvLayer(in_channels, out_channels, kernel_size=kernel_size, padding=padding)
+        self.pool = MaxPool2d(kernel_size=kernel_size)
+
+    def forward(self, x):
+        return self.pool(self.conv(x))
+
 
 class UNetCIFAR10(nn.Module):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.maxpool = MaxPool2d(kernel_size=2, stride=2)
+        self.maxpool = MaxPool2d(kernel_size=2)
+
+        self.enc1 = UNetConvLayer(3, 16)     # 16x16x16
+        self.enc2 = UNetConvLayer(16, 32)    # 32x8x8
+        self.enc3 = UNetConvLayer(32, 64)   # 64x4x4
+
+        self.upconv3 = ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=2, stride=2)
+        self.dec3 = UNetConvLayer(64, 32)
+
+        self.upconv2 = ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=2, stride=2)
+        self.dec2 = UNetConvLayer(32, 16)
+
+        self.upconv1 = ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=2, stride=2)
+        self.dec1 = UNetConvLayer(16, 8)
+
+        self.outconv = Conv2d(in_channels=8, out_channels=3, kernel_size=1)
+
+    def forward(self, x):
+
+        # 16x16x16
+        x1 = self.maxpool(self.enc1(x))
+        print(x1.shape)
+        # 32x8x8
+        x2 = self.maxpool(self.enc2(x1))
+        print(x2.shape)
+        # 64x4x4
+        x3 = self.maxpool(self.enc3(x2))
+        print(x3.shape)
+
+        # stack x2 on up-convolution of x3
+        x2d = self.upconv3(x3)
+
+        x1d = self.dec3(torch.cat((x2, x2d), dim=0))
+        #x1d = self.dec2(torch.stack(x1, self.upconv2(x2d)))
+        #out = self.dec1(torch.stack(x1, self.upconv2(x2d)))
 
 
-        # TODO: completely fix these dimensions and add pads
-        self.enc1 = UNetLayer(3, 8)     # 12x12x8
-        self.enc2 = UNetLayer(8, 16)    # 8x8x16
-        self.enc3 = UNetLayer(16, 32)   # 4x4x32
-        self.enc4 = UNetLayer(32, 64)
 
-        self.enc_mid = UNetLayer(32, 64)
-
-        self.upconv1 = ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=2, stride=2)
-        self.dec1 = UNetLayer(32, 16)
-
-        self.upconv2 = ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=2, stride=2)
-        self.dec2 = UNetLayer(32, 16)
-        self.dec2 = UNetLayer(16, 8)
-        self.dec4 = UNetLayer(8, 64)
 
 
 
 if __name__ == "__main__":
-    test_cifar10_load()
+    # test_cifar10_load()
+    # test_conv_shapes()
+    tensor = torch.randn((3,32,32))
+    unet = UNetCIFAR10()
+    unet(tensor)
--- test/test_ddpm.py
+++ test/test_ddpm.py
@@ -2,6 +2,9 @@ import cifar10
 
 from PIL import Image
 
+import torch
+from torch.nn import Conv2d, MaxPool2d, ConvTranspose2d
+
 def test_cifar10_load():
     """
     Tries loading and saving image from CIFAR10
@@ -15,5 +18,11 @@ def test_cifar10_load():
     im = Image.fromarray(image)
     im.save("test/out/cifar10_test.jpg")
 
+def test_conv_shapes():
+    tensor = torch.randn((2, 32, 32))
+    tensor = ConvTranspose2d(in_channels=2, out_channels=2, kernel_size=2, stride=2)(tensor)
+    #tensor = Conv2d(in_channels=3, out_channels=8, padding=1, kernel_size=2)(tensor)
+    #tensor = MaxPool2d(kernel_size=2)(tensor)
+    print(tensor.shape)
 
 
