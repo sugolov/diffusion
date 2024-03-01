73f530c4d02243b6f08c462e93ac1a71b040fb80
alex n <alexander.neagoe@gmail.com>
2024-02-29 20:46:22 -0500

    added json

--- train/train_ddpm.py
+++ train/train_ddpm.py
@@ -5,3 +5,5 @@ from torch.utils.data import DataLoader
 dataset_train = datasets.CIFAR10('../data', train=True, download=True, transform=transforms.Compose([transforms.ToTensor()]))
 
 training_data_loader = DataLoader(dataset_train, batch_size=64, shuffle=True)
+
+epochs = 1
