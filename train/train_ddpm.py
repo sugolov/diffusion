2e53f6db0193f18a52d9eec6965b1b08c0b0f232
anton <asugolov@gmail.com>
2024-03-01 18:04:06 -0500

    cleaned up, added train todo

--- train/train_ddpm.py
+++ train/train_ddpm.py
@@ -1,4 +1,3 @@
-
 import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
@@ -6,7 +5,6 @@ from torch.utils.data import DataLoader
 from models.ddpm import DDPMnet
 from utils.utils import timestamp
 
-from datetime import datetime
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(torch.cuda.get_device_name(0))
@@ -14,31 +12,26 @@ print(torch.cuda.get_device_name(0))
 
 def train_cifar10(lr=2e-4, epochs=1, batch_size=64):
 
-    dataset_train = datasets.CIFAR10(
-        '../data',
-        train=True,
-        download=True,
-        transform=transforms.Compose([transforms.ToTensor()])
-    )
-
-
+    # CIFAR10 training data
+    dataset_train = datasets.CIFAR10('../data', train=True, download=True,
+                                     transform=transforms.Compose([transforms.ToTensor()]))
     training_data_loader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
+
+    # noise network
     ddpmnet = DDPMnet()
 
     # halving tensor size
     ddpmnet = ddpmnet.to(device)
     ddpmnet.half()
 
-    #for layer in ddpmnet.modules():
-    #    if isinstance(layer, torch.nn.GroupNorm):
-    #        layer.half()
-
-
+    # optimization
     optimizer = torch.optim.Adam(ddpmnet.parameters(), lr=lr)
     MSE = torch.nn.functional.mse_loss
 
+    # training loop
     for i in range(epochs):
 
+        # timestample
         timestamp(i+1)
 
         for batch, pred in training_data_loader:
@@ -58,10 +51,9 @@ def train_cifar10(lr=2e-4, epochs=1, batch_size=64):
             loss.backward()
             optimizer.step()
 
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    print("end: " + str(current_time))
+    timestamp("end")
 
+# TODO: setup checkpointing, https://pytorch.org/tutorials/recipes/recipes/saving_and_loading_a_general_checkpoint.html
 
 if __name__ == "__main__":
     train_cifar10(batch_size=128)
